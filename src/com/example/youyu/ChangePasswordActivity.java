edf065a7f67793b69e76a53e092cb5d17754999c
a542557697 <542557697@qq.com>
2016-04-07 17:30:23 +0800

    修改个人资料和修改密码页面

---
+++ src/com/example/youyu/ChangePasswordActivity.java
@@ -0,0 +1,17 @@
+package com.example.youyu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 修改密码
+ * @author Administrator
+ *
+ */
+public class ChangePasswordActivity extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_change_password);
+	}
+}
---
+++ src/com/example/youyu/ChangePersonalDataActivity.java
@@ -0,0 +1,18 @@
+package com.example.youyu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 修改个人资料
+ * @author Administrator
+ *
+ */
+public class ChangePersonalDataActivity extends Activity{
+
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_change_personaldata);
+	}
+}
