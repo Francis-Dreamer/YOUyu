29ba4fe42db3f72cc582c9f8fc4b6716bd033fff
Francis-Dreamer <534227772@qq.com>
2016-04-14 12:01:46 +0800

    添加 公共 的请求地址属性 在 application

---
+++ src/com/example/youyu/DataApplication.java
@@ -0,0 +1,16 @@
+package com.example.youyu;
+
+import android.app.Application;
+
+public class DataApplication extends Application {
+	/**
+	 * 请求地址的地址头
+	 */
+	public final String url_top = "http://211.149.198.8:9805/";
+	
+	/**
+	 * 请求地址的属性
+	 */
+	public final String url_type = "travel/api/";
+	
+}
